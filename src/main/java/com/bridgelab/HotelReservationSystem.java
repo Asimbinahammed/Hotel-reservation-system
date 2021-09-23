3c499658979d68b385fdebdee1ef6b7cd07a693b
asimbinahammed <asim242370@gmail.com>
2021-09-23 08:33:05 +0530

    welcome message

---
+++ src/main/java/com/bridgelab/HotelReservationSystem.java
@@ -0,0 +1,12 @@
+/*
+@Author:ASIM AHAMMED
+ */
+
+package com.bridgelab;
+
+public class HotelReservationSystem {
+    //main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel reservation system");
+    }
+}
